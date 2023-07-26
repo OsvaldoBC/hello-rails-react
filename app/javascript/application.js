0831993806f690c228dd8ee550d237c0d2b88515
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-07-26 10:57:47 -0500

    Add React dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <div>Hello World</div>;
+    }
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
+    );
